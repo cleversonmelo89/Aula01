5c1e6cc4dbcb568dd335f287bb7e441009b2ec57
Heider Pinholi Lopes <heider.lopes@b2wdigital.com>
2016-08-04 09:13:27 -0300

    Arrumado frase de retorno o painel

--- Saudacao.java
+++ Saudacao.java
@@ -9,12 +9,12 @@ class Saudacao {
      // Scanner entrada = new Scanner(System.in);
 
       String nome = JOptionPane.showInputDialog( "Digite seu nome");
-      JOptionPane.showMessageDialog(null, nome);
+      JOptionPane.showMessageDialog(null, nome + " Bem vindo ao curso");
       //Aqui criamos o objeto que irá receber o nome do aluno
      // String nome = entrada.nextLine();
 
-      System.out.println(nome
-         + " Bem vindo ao curso ");
+     // System.out.println(nome
+     //    + " Bem vindo ao curso ");
 
    }
 
